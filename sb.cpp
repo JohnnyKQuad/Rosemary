f8a926465aeda48537346f862bd9df40c4e48a98
JohnnyKQuad <42100735+JohnnyKQuad@users.noreply.github.com>
2018-08-10 15:14:35 -0500

    Update sb.cpp

--- sb.cpp
+++ sb.cpp
@@ -6,22 +6,44 @@
  * 3x + 5y = n
  * 3x = n-5y --> x=n-5y / 3
  * 
- * y = (n-3x)/5
- */
+ * ax+by=n
+ *. a=3 b=5
+ * ax=n-by
+ * x=(n-by)a
+ * y=(n-ax)/b
+ *
+ * 
  
- for(int i=0;i< ;i++){
-     
- 
- if(n-(3*i))%5==0){
-     x=3*i;
-     y=(n-x)/5;
-     while(x--)
-     str += "555";
-     while(y--)
-     str += "33333";
-     return str;
- }
- 
- return "-1\n";
- }
- 
+  a=3;b=5;
+  
+  x=(n-(b*y)/a);
+  y=(n-(a*x)/b);*/
+  
+  
+int x,y;
+string str="";
+x=0;y=0;
+  
+  for(int i = n/3;i>=0;i--){
+      if(i*3==n)
+      {
+          x=i;y=0;break;}
+      
+      if((n-(i*3))%5==0){
+          x=i;
+          y=(n-(i*3))/5;
+          break;
+      }
+      
+  }
+  if(x||y){
+  while(x--)
+    str+="555";
+  while(y--)
+    str+="33333";
+    }
+  else {
+      str=="-1";
+  }
+  cout<<str<<"\n";
+  }
