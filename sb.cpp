f6bceb4e045b9d562d6b5e8b8dd9d9b854d1faeb
JohnnyKQuad <jonkstewart@gmail.com>
2018-08-10 00:55:30 -0500

    committed

---
+++ sb.cpp
@@ -0,0 +1,27 @@
+ 
+
+
+
+/* ax + by = n 
+ * 3x + 5y = n
+ * 3x = n-5y --> x=n-5y / 3
+ * 
+ * y = (n-3x)/5
+ */
+ 
+ for(int i=0;i< ;i++){
+     
+ 
+ if(n-(3*i))%5==0){
+     x=3*i;
+     y=(n-x)/5;
+     while(x--)
+     str += "555";
+     while(y--)
+     str += "33333";
+     return str;
+ }
+ 
+ return "-1\n";
+ }
+ 
