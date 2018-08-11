5a7c17e6b048a0f3ecf8bd355b436045cac95457
JohnnyKQuad <jonkstewart@gmail.com>
2018-08-10 20:20:24 -0500

    pri+

--- pri.cpp
+++ pri.cpp
@@ -1,1 +1,13 @@
+w.sort();
 
+std::vector<int>::iterator it0 = w.begin();
+    curr = w.begin; j=0;
+   for(int i=w.begin() ; i!=w.end() ; i++)
+   {
+       if (i<curr+4){}
+       else{
+           curr=i;
+           j++;
+       }
+   }
+   return j;
